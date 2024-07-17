ab729cf3b9d3207f1889704ba833379ad4680d9b
shaheerahmedkhan11 <shaheerahmedkhan11@gmail.com>
2024-07-17 18:58:59 +0500

    Initial Commit

---
+++ TagManager.cs
@@ -0,0 +1,9 @@
+﻿internal class TagManager
+{
+
+    public static string PLAYER_TAG = "Player", ENEMY_TAG = "Enemy", BULLET_TAG = "Bullet",
+    GAMEPLAY_CANVAS_TAG = "GamePlayCanvas";
+    public static string HORIZONTAL_AXIS = "Horizontal", VERTICAL_AXIS = "Vertical";
+    public static string IDLE_ANIMATION_NAME = "Idle", WALK_ANIMATION_NAME = "Walk", SHOOT_ANIMATION_NAME = "Shoot", ATTACK_ANIMATION_NAME = "Name", DEATH_ANIMATION_NAME = "Death";
+
+}
